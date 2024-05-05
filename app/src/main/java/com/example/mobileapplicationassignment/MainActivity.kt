11ccb7d788407ee3cc433e77d17e13c68a9531dd
Wong-Cheng-Yi <wongchengyi911@gmail.com>
2024-05-05 17:03:42 +0800

    Update

--- app/src/main/java/com/example/mobileapplicationassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobileapplicationassignment/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.mobileapplicationassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dbRef : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,5 +16,14 @@ class MainActivity : AppCompatActivity() {
         //lol
         //XinYi test test test
         //Hi my name is wong chen yong
+        dbRef = FirebaseDatabase.getInstance().getReference("Product")
+        dbRef.child("Testing").setValue("Hello World")
+            .addOnCompleteListener{
+                Toast.makeText(this, "Data saved", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Error ${it.toString()}", Toast.LENGTH_LONG).show()
+            }
+
     }
 }
