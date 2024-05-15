7c389a97e5f22cac259b909a9f98b983d6ccdfca
Asus <1181203214@student.mmu.edu.my>
2024-05-15 17:47:15 +0800

    15/5/2024

--- app/src/main/java/com/example/mobileapplicationassignment/CheckoutFragment.kt
+++ app/src/main/java/com/example/mobileapplicationassignment/CheckoutFragment.kt
@@ -1,34 +1,21 @@
 package com.example.mobileapplicationassignment
 
-import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileapplicationassignment.data.Product
 import com.example.mobileapplicationassignment.dataAdapter.CartAdapter
-import com.example.mobileapplicationassignment.dataAdapter.VPAdapter
-import com.example.mobileapplicationassignment.dataAdapter.VPHAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.mobileapplicationassignment.dataAdapter.CheckoutAdapter
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.getValue
-import com.google.firebase.storage.FirebaseStorage
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,10 +24,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ShoppingCartFragment.newInstance] factory method to
+ * Use the [CheckoutFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ShoppingCartFragment : Fragment() {
+class CheckoutFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -60,27 +47,16 @@ class ShoppingCartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_shopping_cart, container, false)
-        // Inflate the layout for this fragment
-        var recyclerView: RecyclerView = view.findViewById(R.id.CartRecycleView)
-        var checkout: Button = view.findViewById(R.id.CheckoutBtn)
-        // Create a storage reference from our app
+        var view = inflater.inflate(R.layout.fragment_checkout, container, false)
+
+        var recyclerView: RecyclerView = view.findViewById(R.id.CheckoutRecyclerView)
         val id = arguments?.getString("id").toString()
         dbRef = FirebaseDatabase.getInstance().getReference("User").child(id)
         //val person = Person("P002", "Yashiro", "gs://fir-622cc.appspot.com/myImg/Yashiro.png")
 
         fetchData(recyclerView)
 
-        checkout.setOnClickListener{
-            val fragment = CheckoutFragment()
-            val bundle = Bundle()
-            bundle.putString("id",id)
-            fragment.arguments = bundle
-            val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.fragmentContainerView, fragment)
-            transaction?.addToBackStack(null)
-            transaction?.commit()
-        }
+
         return view
     }
 
@@ -96,7 +72,7 @@ class ShoppingCartFragment : Fragment() {
                         productList.add(product!!)
                     }
                 }
-                recyclerView.adapter = CartAdapter(productList)
+                recyclerView.adapter = CheckoutAdapter(productList)
                 recyclerView.layoutManager = LinearLayoutManager(requireContext())
                 recyclerView.setHasFixedSize(true)
             }
@@ -105,5 +81,6 @@ class ShoppingCartFragment : Fragment() {
             }
         })
     }
+
 
 }
---
+++ app/src/main/java/com/example/mobileapplicationassignment/dataAdapter/CheckoutAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.mobileapplicationassignment.dataAdapter
+
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobileapplicationassignment.R
+import com.example.mobileapplicationassignment.data.Product
+import com.google.firebase.storage.FirebaseStorage
+
+class CheckoutAdapter(private val productList: List<Product>) : RecyclerView.Adapter<CheckoutAdapter.CheckoutViewHolder>(){
+
+    class CheckoutViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView){
+        val CoName : TextView = itemView.findViewById(R.id.checkoutProductName)
+        val CoPrice: TextView = itemView.findViewById(R.id.checkoutProductPrice)
+        val CoImage: ImageView =itemView.findViewById(R.id.checkoutProductImg)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheckoutViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.checkout_view_holder, parent, false )
+
+        return CheckoutAdapter.CheckoutViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CheckoutAdapter.CheckoutViewHolder, position: Int) {
+        val currentItem = productList[position]
+        if (currentItem.image.isNotEmpty()) {
+            var imgRef = FirebaseStorage.getInstance().getReferenceFromUrl(currentItem.image)
+            //holder.cName.id = currentItem.id
+            holder.CoName.text = currentItem.name
+            //holder.vDesc.text = currentItem.description
+            holder.CoPrice.text = currentItem.price.toString()
+//            if(currentItem.status == true){
+//                holder.vStatus.text = "Unsold"
+//            }else{
+//                holder.vStatus.text = "Sold"
+//            }
+            val ONE_MEGABYTE: Long = 1024 * 1024
+            imgRef.getBytes(ONE_MEGABYTE)
+                .addOnSuccessListener { bytes ->
+                    // Convert the bytes to a Bitmap
+                    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    // Display the Bitmap in an ImageView
+                    holder.CoImage.setImageBitmap(bitmap)
+                }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+}
